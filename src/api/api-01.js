b584685e6fe400bef356b9f457373dc55148201d
vicweiyu <vic.weiyu@gmail.com>
2021-08-16 17:14:12 +0930

    cli

---
+++ src/api/api-01.js
@@ -0,0 +1,19 @@
+const babelCore = require('@babel/core');
+
+const es6Code = `const f = () => { console.log('Hello'); }`;
+const options = {
+  presets: ['@babel/preset-env'],
+};
+
+const result = babelCore.transform(es6Code, options);
+console.log(result);
+console.log('----------');
+console.log(result.code);
+
+/*
+babelCore.transform(es6Code, options, (err, result) => {
+  console.log(result);
+  console.log('----------');
+  console.log(result.code);
+});
+*/
